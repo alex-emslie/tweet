e2c249325aa41dfdd807fe4da91489fde49e4b5c
Alex Emslie <aremslie@gmail.com>
2025-06-06 08:50:38 -0700

    fix build config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
   },
+  output: 'standalone',
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
